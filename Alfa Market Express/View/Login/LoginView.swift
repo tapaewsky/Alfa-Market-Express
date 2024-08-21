01a6b2671e57df1df2c89bebd1fd56f8b07ffe88
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-08-22 02:12:33 +0300

    Add LoginView and Authorization with jwt token

---
+++ Alfa Market Express/View/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  Log In.swift
+//  Alfa Market Express
+//
+//  Created by Said Tapaev on 22.08.2024.
+//
+
+import SwiftUI
+
+struct Log_In: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Log_In()
+}
---
+++ Alfa Market Express/ViewModel/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  Alfa Market Express
+//
+//  Created by Said Tapaev on 22.08.2024.
+//
+
+import Foundation
