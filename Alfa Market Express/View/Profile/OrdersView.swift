2dbe714f82f03fc941106d31c6979a12469a3e51
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-09-25 19:15:20 +0300

    update ProfileView fnd add EditProfile!

---
+++ Alfa Market Express/View/Profile/OrdersView.swift
@@ -0,0 +1,18 @@
+//
+//  OrdersView.swift
+//  Alfa Market Express
+//
+//  Created by Said Tapaev on 25.09.2024.
+//
+
+import SwiftUI
+
+struct OrdersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrdersView()
+}
---
+++ Alfa Market Express/ViewModel/OrdersViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OrdersViewModel.swift
+//  Alfa Market Express
+//
+//  Created by Said Tapaev on 25.09.2024.
+//
+
+import Foundation
