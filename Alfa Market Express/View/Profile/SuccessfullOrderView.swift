fbf81f653c84f61e37a903a4b6bae34eabceb8ff
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-10-24 03:00:45 +0300

    поработал над успешным заказом

--- Alfa Market Express/View/Profile/SuccessfullOrderView.swift
+++ Alfa Market Express/View/Profile/SuccessfullOrderView.swift
@@ -9,59 +9,74 @@ import SwiftUI
 
 struct SuccessfullOrderView: View {
     @State private var showOrders = false
-    @State private var showCart = false
     @StateObject var viewModel: MainViewModel
+    @State private var showHome = false
     
     var body: some View {
         VStack {
-            VStack(spacing: 16) {
-                Image(systemName: "checkmark.circle.fill")
-                    .font(.title)
-                    .foregroundColor(.colorGreen)
+            Image(systemName: "checkmark.circle.fill")
+                .font(.system(size: 150))
+                .foregroundColor(.colorGreen)
+            
+            
+            Spacer()
+            
+            VStack(spacing: 10) {
                 Text("Вы успешно сделали заказ!")
-                    .font(.headline)
+                    .font(.title2)
                     .foregroundColor(.black)
+                    .bold()
                 Text("Вы можете просмотреть свои заказы.")
-                    .font(.headline)
+                    .font(.subheadline)
                     .foregroundColor(.black)
-                
-                Button(action: {
-                    showOrders = true
-                }) {
-                    Text("Мои заказы")
-                        .padding(.vertical)
-                        .padding(.horizontal, 20)
-                        .font(.headline)
-                        .foregroundColor(.black)
-                        .cornerRadius(20)
-                        .bold()
-                }
-                .background(Color.white)
-                .cornerRadius(20)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(Color.colorGreen, lineWidth: 2)
-                )
-                .background(
-                    NavigationLink(destination: OrdersView(viewModel: viewModel), isActive: $showOrders) {
-                        EmptyView()
-                    }
-                       
-                    
-                    
-                )
             }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(12)
-            .shadow(radius: 2)
+            
+            Spacer()
             
             
+            Button(action: {
+                showOrders = true
+            }) {
+                Text("Мои заказы")
+                    .padding(.vertical)
+                    .padding(.horizontal, 140)
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                    .background(Color.colorGreen)
+                    .cornerRadius(15)
+                    .bold()
+            }
+            .background(
+                NavigationLink(destination: OrdersView(viewModel: viewModel), isActive: $showOrders) {
+                    EmptyView()
+                }
+            )
+
+           
+            Button(action: {
+                showHome = true
+            }) {
+                Text("Главная")
+                    .padding(.vertical)
+                    .padding(.horizontal, 155)
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                    .background(Color.colorGreen)
+                    .cornerRadius(15)
+                    .bold()
+            }
+            .background(
+                NavigationLink(destination: HomeView(viewModel: viewModel), isActive: $showHome) {
+                    EmptyView()
+                }
+            )
         }
+        .padding(.top, 150)
+        .padding(.bottom, 100)
         .navigationBarBackButtonHidden(true)
     }
-    
 }
+
 struct SuccessfullOrderView_Preview: PreviewProvider {
     static var previews: some View {
         SuccessfullOrderView(viewModel: MainViewModel())
