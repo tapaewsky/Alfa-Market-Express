0e94450da1eed80a97b9ec4fc27f304cc70ab01d
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-10-29 01:10:45 +0300

    add category (all prouduct)

--- Alfa Market Express/View/Category/CategoryProductsView.swift
+++ Alfa Market Express/View/Category/CategoryProductsView.swift
@@ -15,7 +15,7 @@ struct CategoryProductsView: View {
     var body: some View {
         VStack {
             if let category = selectedCategory {
-               
+
                 HStack {
                     Button(action: {
                         selectedCategory = nil
@@ -28,29 +28,30 @@ struct CategoryProductsView: View {
                         }
                     }
                     .padding()
-                    
+
                     Spacer()
                 }
-                
+
                 ScrollView {
-                    if viewModel.productViewModel.products.isEmpty && !isFetching {
+                    let filteredProducts = category.id == 0
+                        ? viewModel.productViewModel.products
+                        : viewModel.productViewModel.products.filter { $0.category == category.id }
+                    
+                    if filteredProducts.isEmpty && !isFetching {
                         Text("Нет доступных продуктов для категории: \(category.name)")
                             .padding()
                     } else {
                         ProductGridView(
                             viewModel: viewModel,
-                            products: viewModel.productViewModel.products.filter { $0.category == category.id },
+                            products: filteredProducts,
                             onFavoriteToggle: { _ in }
                         )
                     }
                 }
-//                .onAppear {
-//                    loadProducts(for: category)
-//                }
 
             } else {
                 LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 1)], spacing: 10) {
-                    ForEach(viewModel.categoryViewModel.categories) { category in
+                    ForEach(viewModel.categoryViewModel.categories, id: \.id) { category in
                         Button(action: {
                             selectedCategory = category
                         }) {
@@ -62,19 +63,5 @@ struct CategoryProductsView: View {
             }
         }
     }
-
-//    private func loadProducts(for category: Category) {
-//        guard !isFetching else { return }
-//        isFetching = true
-//        viewModel.productViewModel.fetchProducts { success in
-//            DispatchQueue.main.async {
-//                isFetching = false
-//                if success {
-//                    print("Продукты успешно загружены для категории \(category.name)")
-//                } else {
-//                    print("Не удалось загрузить продукты для категории \(category.name)")
-//                }
-//            }
-//        }
-//    }
 }
+
