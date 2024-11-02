ed07cd164b7637c641247a134c075a5c4a8788a2
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-11-03 01:34:28 +0300

    comment for catalogView

--- Alfa Market Express/View/Catalog/CatalogCardView.swift
+++ Alfa Market Express/View/Catalog/CatalogCardView.swift
@@ -5,26 +5,26 @@
 //  Created by Said Tapaev on 07.09.2024.
 //
 
-import SwiftUI
-import Kingfisher
-
-struct CatalogCardView: View {
-    let category: Category
-
-    var body: some View {
-        ZStack(alignment: .top) {
-            KFImage(URL(string: category.imageUrl ?? "https://example.com/placeholder.png"))
-                .placeholder {
-                    ProgressView()
-                }
-                .resizable()
-                .scaledToFill()
-            Text(category.name)
-                .font(.caption)
-                .foregroundStyle(.black)
-                
-        }
-        .padding(0)
-       
-    }
-}
+//import SwiftUI
+//import Kingfisher
+//
+//struct CatalogCardView: View {
+//    let category: Category
+//
+//    var body: some View {
+//        ZStack(alignment: .top) {
+//            KFImage(URL(string: category.imageUrl ?? "https://example.com/placeholder.png"))
+//                .placeholder {
+//                    ProgressView()
+//                }
+//                .resizable()
+//                .scaledToFill()
+//            Text(category.name)
+//                .font(.caption)
+//                .foregroundStyle(.black)
+//                
+//        }
+//        .padding(0)
+//       
+//    }
+//}
