a2355e98e17b0e5b7a425b2aef230c39bcbde0fb
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-12-03 02:47:24 +0300

    add pagination for homeView

--- Alfa Market Express/View/Home/HomeView.swift
+++ Alfa Market Express/View/Home/HomeView.swift
@@ -4,65 +4,7 @@
 //
 //  Created by Said Tapaev on 06.07.2024.
 //
-//import SwiftUI
-//import Combine
-//
-//struct HomeView: View {
-//    @StateObject var viewModel: MainViewModel
-//    @StateObject private var networkMonitor = NetworkMonitor()
-//    @State private var shuffledProducts: [Product] = []
-//    @State private var currentPage: Int = 1
-//    @State var isFetching = false
-//  
-//    var body: some View {
-//        NavigationView {
-//            ZStack {
-//                ScrollView(showsIndicators: false) {
-//                    SlidesCardView(viewModel: viewModel)
-//                    SearchBar(viewModel: viewModel)
-//                    ProductGridView(viewModel: viewModel, products: shuffledProducts, onFavoriteToggle: { _ in })
-//                        .padding(.vertical)
-//                }
-//            }
-//        }
-//        .navigationBarBackButtonHidden(true)
-//        .onAppear {
-//            loadProducts()
-//        }
-//    }
-//    
-//    private func loadProducts() {
-//        isFetching = true
-//        let productFetchGroup = DispatchGroup()
-//        
-//        productFetchGroup.enter()
-//        viewModel.productViewModel.fetchProducts() { success in
-//            DispatchQueue.main.async {
-//                if success {
-//                    self.updateShuffledProducts()
-//                } else {
-//                    print("Не удалось загрузить главное")
-//                }
-//                productFetchGroup.leave()
-//            }
-//        }
-//        
-//        productFetchGroup.enter()
-//        viewModel.slideViewModel.fetchSlides { success in
-//            DispatchQueue.main.async {
-//                if success {
-//                } else {
-//                    print("Не удалось загрузить слайды")
-//                }
-//                productFetchGroup.leave()
-//            }
-//        }
-//    }
-//
-//    private func updateShuffledProducts() {
-//        shuffledProducts = viewModel.productViewModel.products.shuffled()
-//    }
-//}
+
 import SwiftUI
 
 struct HomeView: View {
@@ -70,101 +12,99 @@ struct HomeView: View {
     @State private var isFetching: Bool = false
     @State private var hasMoreData: Bool = true
     @State private var currentPage: Int = 1
-    @State private var shuffledProducts: [Product] = []
-
-
+    
     var body: some View {
-            NavigationView {
-                ZStack {
-                    ScrollView(showsIndicators: false) {
-                        SlidesCardView(viewModel: viewModel)
-                        SearchBar(viewModel: viewModel)
-                        productList
-                            .padding(.vertical)
-                    }
+        NavigationView {
+            ZStack {
+                ScrollView(showsIndicators: false) {
+                    SlidesCardView(viewModel: viewModel)
+                    SearchBar(viewModel: viewModel)
+                    productList
+                        .padding(.vertical)
                 }
             }
             .navigationBarBackButtonHidden(true)
             .onAppear {
                 loadInitialProducts()
-                
             }
         }
+    }
+
     private var productList: some View {
-        ScrollView {
-            LazyVStack {
-                if shuffledProducts.isEmpty && !isFetching {
-                    Text("Нет доступных продуктов.")
-                        .padding()
-                } else {
-                    ProductGridView(
-                        viewModel: viewModel,
-                        products: shuffledProducts,
-                        onFavoriteToggle: { _ in }
-                    )
-                }
-
-                if isFetching {
-                    ProgressView()
-                        .padding()
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
-
-                if hasMoreData {
-                    GeometryReader { proxy in
-                        Color.clear
-                            .frame(height: 1)
-                            .onAppear {
-                                if !isFetching {
-                                    if proxy.frame(in: .global).maxY <= UIScreen.main.bounds.height {
-                                        print("Загружаем следующую страницу... Текущая страница: \(currentPage)")
-                                        loadMoreProducts()
-                                    }
-                                }
+        LazyVStack {
+            if viewModel.productViewModel.products.isEmpty && !isFetching {
+                Text("Нет доступных продуктов.")
+                    .padding()
+            } else {
+                ProductGridView(
+                    viewModel: viewModel,
+                    products: viewModel.productViewModel.products,
+                    onFavoriteToggle: { _ in }
+                )
+            }
+            
+            if isFetching {
+                ProgressView()
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+            
+            if hasMoreData {
+                GeometryReader { proxy in
+                    Color.clear
+                        .frame(height: 1)
+                        .onAppear {
+                            if proxy.frame(in: .global).maxY <= UIScreen.main.bounds.height {
+                                loadMoreProducts()
                             }
-                    }
-                    .frame(height: 1)
+                        }
                 }
+                .frame(height: 1)
             }
         }
     }
     
     private func loadInitialProducts() {
-            isFetching = true
-            let productFetchGroup = DispatchGroup()
-    
-            productFetchGroup.enter()
-            viewModel.productViewModel.fetchProducts() { success in
-                DispatchQueue.main.async {
-                    if success {
-                        self.updateShuffledProducts()
-                    } else {
-                        print("Не удалось загрузить главное")
-                    }
-                    productFetchGroup.leave()
+        guard !isFetching else { return }
+        isFetching = true
+        
+        let group = DispatchGroup()
+        
+        group.enter()
+        viewModel.productViewModel.fetchProducts { success in
+            DispatchQueue.main.async {
+                if !success {
+                    print("Не удалось загрузить продукты")
                 }
-            }
-    
-            productFetchGroup.enter()
-            viewModel.slideViewModel.fetchSlides { success in
-                DispatchQueue.main.async {
-                    if success {
-                    } else {
-                        print("Не удалось загрузить слайды")
-                    }
-                    productFetchGroup.leave()
-                }
+                group.leave()
             }
         }
-
+        
+        group.enter()
+        viewModel.slideViewModel.fetchSlides { success in
+            DispatchQueue.main.async {
+                if !success {
+                    print("Не удалось загрузить слайды")
+                }
+                group.leave()
+            }
+        }
+        
+        group.notify(queue: .main) {
+            isFetching = false
+        }
+    }
+    
     private func loadMoreProducts() {
-        guard !isFetching else { return }
+        guard !isFetching, hasMoreData else { return }
+        
         isFetching = true
         viewModel.productViewModel.fetchProducts { success in
             DispatchQueue.main.async {
                 if success {
-                    updateShuffledProducts()
-                    currentPage += 1
+                    if viewModel.productViewModel.baseURL == nil {
+                        hasMoreData = false
+                    }
                 } else {
                     print("Не удалось загрузить следующую страницу")
                     hasMoreData = false
@@ -172,9 +112,5 @@ struct HomeView: View {
                 isFetching = false
             }
         }
-    }
-
-    private func updateShuffledProducts() {
-        shuffledProducts = viewModel.productViewModel.products
     }
 }
